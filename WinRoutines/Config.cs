e09922f8bba4e27ddc3e9d8054fdd9129270bf58
Sergey Stoyan <sergey.stoyan@gmail.com>
2020-03-06 14:41:25 +0200

    1.2.0

---
+++ WinRoutines/Config.cs
@@ -0,0 +1,35 @@
+﻿//********************************************************************************************
+//Author: Sergey Stoyan
+//        sergey.stoyan@gmail.com
+//        sergey_stoyan@yahoo.com
+//        http://www.cliversoft.com
+//        26 September 2006
+//Copyright: (C) 2006, Sergey Stoyan
+//********************************************************************************************
+using System.IO;
+using System.Security.AccessControl;
+
+namespace Cliver.Win
+{
+    abstract public class AppSettings : Settings
+    {
+        /// <summary>
+        /// By defult Environment.SpecialFolder.CommonApplicationData does not have writting permission
+        /// </summary>
+        /// <param name="userIdentityName"></param>
+        public static void AllowReadWriteConfig(string userIdentityName = null)
+        {
+            if (userIdentityName == null)
+                userIdentityName = UserRoutines.GetCurrentUserName3();
+            DirectoryInfo di = new DirectoryInfo(Cliver.AppSettings.StorageDir);
+            DirectorySecurity ds = di.GetAccessControl();
+            ds.AddAccessRule(new FileSystemAccessRule(
+                userIdentityName,
+                FileSystemRights.Modify,
+                InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit,
+                PropagationFlags.None,
+                AccessControlType.Allow));
+            di.SetAccessControl(ds);
+        }
+    }
+}
