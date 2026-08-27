40a9d76f4ab4b768f50c2eeab89f9563aa43dd80
Sergiy Stoyan <s.y.stoyan@gmail.com>
2026-08-27 14:50:54 +0300

    3.0.1
    - version info fixed;
    - version 4 octet auto increment added;

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,8 +13,9 @@ namespace Test
         static void Main()
         {
             try
-            {
-                throw new Exception("test");
+            {//DateTime.Now.GetSecondsSinceUnixEpoch
+                string f = Log.GetReferencedAssembliesInfo(nameof(Cliver));
+                                                throw new Exception(f);
 
                 // To customize application configuration such as set high DPI settings or default font,
                 // see https://aka.ms/applicationconfiguration.
